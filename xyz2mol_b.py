e1f50c8da4643c45987a7b72dfc5d66c0e1a973c
Norwid Behrnd <nbehrnd@yahoo.com>
2025-01-30 19:23:31 +0100

    style(xyz2mol_b.py): apply pre-commit hook
    Signed-off-by: Norwid Behrnd <nbehrnd@yahoo.com>

--- xyz2mol_b.py
+++ xyz2mol_b.py
@@ -19,22 +19,26 @@ def get_args():
     """get arguments from the CLI"""
 
     parser = argparse.ArgumentParser(
-        description=
-        "convert a xyz geometry into a .sdf file with RDKit 2023.03.2, or greater",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="convert a xyz geometry into a .sdf file with RDKit 2023.03.2, or greater",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument("file",
-                        help=".xyz input file to process",
-                        metavar="FILE",
-                        type=argparse.FileType("rt"),
-                        default=None)
+    parser.add_argument(
+        "file",
+        help=".xyz input file to process",
+        metavar="FILE",
+        type=argparse.FileType("rt"),
+        default=None,
+    )
 
-    parser.add_argument("-c",
-                        "--charge",
-                        help="charge globally attributed to the structure",
-                        metavar="int",
-                        type=int,
-                        default=0)
+    parser.add_argument(
+        "-c",
+        "--charge",
+        help="charge globally attributed to the structure",
+        metavar="int",
+        type=int,
+        default=0,
+    )
 
     return parser.parse_args()
 
